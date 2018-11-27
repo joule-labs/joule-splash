2cf57b9e3abb2913bbc515fe6008c3c248a3af43
Will O'Beirne <wbobeirne@gmail.com>
2018-11-27 17:24:54 -0500

    Install links to github releases.

--- js/script.js
+++ js/script.js
@@ -13,15 +13,18 @@ $(function() {
     navigator.userAgent.search("OPR") >= 0
   ) {
     browser = 'opera';
-    installUrl = '#opera';
+    // TODO: Update with Opera extension store URL.
+    installUrl = 'https://github.com/wbobeirne/joule-extension/releases';
   }
   else if (navigator.userAgent.search("Chrome") >= 0) {
     browser = 'chrome';
-    installUrl = '#chrome';
+    // TODO: Update with Chrome extension store URL.
+    installUrl = 'https://github.com/wbobeirne/joule-extension/releases';
   }
   else if (navigator.userAgent.search("Firefox") >= 0) {
     browser = 'firefox';
-    installUrl = '#firefox';
+    // TODO: Update with Firefox extension store URL.
+    installUrl = 'https://github.com/wbobeirne/joule-extension/releases';
   }
 
   $('body').removeClass('no-browser');
