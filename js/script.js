cbdf8c68722bcaf5acda6f8d2ed1a22921d2ec96
Will O'Beirne <wbobeirne@gmail.com>
2018-12-26 11:25:07 -0500

    Point to extension stores and official twitter. Minor style tweaks.

--- js/script.js
+++ js/script.js
@@ -19,12 +19,12 @@ $(function() {
   else if (navigator.userAgent.search("Chrome") >= 0) {
     browser = 'chrome';
     // TODO: Update with Chrome extension store URL.
-    installUrl = 'https://github.com/wbobeirne/joule-extension/releases';
+    installUrl = 'https://chrome.google.com/webstore/detail/joule/aejmoogjdllanidlpfjmmmmimfaficio';
   }
   else if (navigator.userAgent.search("Firefox") >= 0) {
     browser = 'firefox';
     // TODO: Update with Firefox extension store URL.
-    installUrl = 'https://github.com/wbobeirne/joule-extension/releases';
+    installUrl = 'https://addons.mozilla.org/en-US/firefox/addon/lightning-joule/';
   }
 
   $('body').removeClass('no-browser');
